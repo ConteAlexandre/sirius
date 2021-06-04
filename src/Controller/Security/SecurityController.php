0bc819013ad00a18df3fe553ba29100aa03b569c
ConteAlexandre <pro.alexandre.conte@gmail.com>
2021-06-04 16:53:25 +0200

    Update security and create connexion

---
+++ src/Controller/Security/SecurityController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller\Security;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * Class SecurityController
+ *
+ * @author CONTE Alexandre <pro.alexandre.conte@gmail.com>
+ */
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/login", name="login", methods={"POST"})
+     */
+    public function login(AuthenticationUtils $authenticationUtils)
+    {
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return new JsonResponse([$lastUsername, $error]);
+    }
+}
