55518328c0f1e3cc2477bc16f82f47b5bf47ccd4
ConteAlexandre <pro.alexandre.conte@gmail.com>
2021-06-21 10:54:10 +0200

    Initialization admin

---
+++ src/Admin/AdminUser.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+
+/**
+ * Class AdminUser
+ *
+ * @author CONTE Alexandre <pro.alexandre.conte@gmail.com>
+ */
+class AdminUser extends AbstractAdmin
+{
+
+}
