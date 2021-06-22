65ef2dbcaebde9564eb1cc6a84069c36a9f5e566
ConteAlexandre <pro.alexandre.conte@gmail.com>
2021-06-22 10:04:58 +0200

    Update Admin

--- src/Admin/AdminUser.php
+++ src/Admin/AdminUser.php
@@ -3,6 +3,10 @@
 namespace App\Admin;
 
 use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
 
 /**
  * Class AdminUser
@@ -11,5 +15,75 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
  */
 class AdminUser extends AbstractAdmin
 {
+    /**
+     * @param FormMapper $form
+     */
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form
+            ->add('username')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('company')
+            ->add('company')
+            ->add('companyActivity')
+            ->add('email')
+            ->add('phoneNumber')
+            ->add('beverage')
+            ->add('plainPassword')
+            ->add('skills')
+        ;
+    }
 
+    /**
+     * @param ListMapper $list
+     */
+    protected function configureListFields(ListMapper $list)
+    {
+        $list
+            ->addIdentifier('username', null, [
+                'route' => [
+                    'name' => 'show',
+                ]
+            ])
+            ->add('email')
+            ->add('createdAt')
+            ->add('createdBy')
+        ;
+    }
+
+    /**
+     * @param DatagridMapper $filter
+     */
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+            ->add('username')
+            ->add('email')
+        ;
+    }
+
+    /**
+     * @param ShowMapper $show
+     */
+    protected function configureShowFields(ShowMapper $show)
+    {
+        $show
+            ->add('username')
+            ->add('firstName')
+            ->add('lastName')
+            ->add('company')
+            ->add('company')
+            ->add('companyActivity')
+            ->add('email')
+            ->add('phoneNumber')
+            ->add('beverage')
+            ->add('plainPassword')
+            ->add('skills')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('createdBy')
+            ->add('updatedBy')
+        ;
+    }
 }
---
+++ src/Controller/Admin/AdminUserController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class AdminUserController
+ *
+ * @author CONTE Alexandre <pro.alexandre.conte@gmail.com>
+ */
+class AdminUserController extends AbstractController
+{
+    public function createLinkForInscription()
+    {
+        
+    }
+}
