3f27f687d14f27b84379bc79a1db348803450635
BaptisteBobot <baptiste.bobot@gmail.com>
2021-06-25 15:06:20 +0200

    add formbuilder for aperitif route and IdeaBox

--- src/Controller/IdeaBoxController.php
+++ src/Controller/IdeaBoxController.php
@@ -2,30 +2,43 @@
 
 namespace App\Controller;
 
-use App\Entity\IdeaBox;
+use App\Form\IdeaBoxFormType;
+use App\Manager\IdeaBoxManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class IdeaBoxController extends AbstractController
 {
     /**
      * @Route("/ideabox/", name="idea_box", methods={"POST"})
+     * @param IdeaBoxManager     $ideaManager
+     * @param Request            $request
+     * @param ValidatorInterface $validator
+     *
+     * @return JsonResponse
+     * @throws \Exception
      */
-    public function postIdea(Request $request): Response
+    public function postIdea(Request $request, IdeaBoxManager $ideaManager, ValidatorInterface  $validator): Response
     {
-        $response = new Response();
-        $idea = new IdeaBox();
-        $reqData = json_decode($request->getContent(), true);
-        $idea->setContent($reqData["content"]);
+        $data = json_decode($request->getContent(), true);
+        $ideabox = $ideaManager->createIdeaBox();
+        $form = $this->createForm(IdeaBoxFormType::class, $ideabox);
+        $form->submit($data);
 
 
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($idea);
-        $em->flush();
-        $response->setContent($idea->getId());
-        $response->setStatusCode(Response::HTTP_CREATED);
-        return $response;
+        $violation = $validator->validate($ideabox);
+
+        if (count($violation) > 0) {
+            foreach ($violation as $error) {
+                return new JsonResponse($error->getMessage(), Response::HTTP_BAD_REQUEST);
+            }
+        }
+
+        $ideaManager->save($ideabox);
+
+        return new JsonResponse('Idea Box created');
     }
 }
---
+++ src/Manager/IdeaBoxManager.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace App\Manager;
+
+
+use App\Entity\IdeaBox;
+use App\Repository\IdeaBoxRepository;
+use Doctrine\ORM\EntityManagerInterface;
+
+class IdeaBoxManager
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $em;
+
+
+    /**
+     * @var IdeaBoxRepository
+     */
+    protected $clientRepository;
+
+    /**
+     * ClientManager constructor.
+     * @param EntityManagerInterface $entityManager
+     * @param IdeaBoxRepository $ideaboxRepository
+     */
+    public function __construct(EntityManagerInterface $entityManager, IdeaBoxRepository $ideaboxRepository)
+    {
+        $this->em = $entityManager;
+        $this->ideaboxRepository = $ideaboxRepository;
+    }
+
+    /**
+     * @return IdeaBox
+     */
+    public function createIdeaBox(): IdeaBox
+    {
+        $ideabox = new IdeaBox();
+        return $ideabox;
+    }
+
+    /**
+     * @param IdeaBox $ideabox
+     * @param bool $andFlush
+     *
+     * @throws \Exception
+     */
+    public function save(IdeaBox $ideabox, $andFlush = true)
+    {
+        $this->em->persist($ideabox);
+        if ($andFlush) {
+            $this->em->flush();
+        }
+    }
+
+}
